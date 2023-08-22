b16833d26f4093691be8166c642c6c33f9f33d9e
Jelena Petrovic <jelena.petrovic1517@gmail.com>
2023-08-21 22:57:47 -0400

    Create user class, write & read to db

--- src/main/java/com/algonquin/urlshortener/dao/ApplicationDao.java
+++ src/main/java/com/algonquin/urlshortener/dao/ApplicationDao.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.algonquin.urlshortener.beans.ShortenedUrl;
+import com.algonquin.urlshortener.beans.User;
 
 public class ApplicationDao {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
@@ -109,6 +110,50 @@ public class ApplicationDao {
         }
     }
     
+    public void insertUser(User user) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            Statement statement = connection.createStatement();
+            String useDatabaseQuery = "USE " + DB_NAME;
+            statement.executeUpdate(useDatabaseQuery);
+        	String query = "INSERT INTO users (email, password) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getPassword());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+    public User getUserByEmail(String email) {
+        User user = null;
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            Statement statement = connection.createStatement();
+            String useDatabaseQuery = "USE " + DB_NAME;
+            statement.executeUpdate(useDatabaseQuery);
+        	
+        	String query = "SELECT * FROM users WHERE email = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String fetchedEmail = resultSet.getString("email");
+                String password = resultSet.getString("password");
+
+                user = new User(id, fetchedEmail, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+    
     private void createDbIfRequired() {
     	try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
             // Create the database if it doesn't exist
@@ -119,8 +164,16 @@ public class ApplicationDao {
             // Use the newly created database
             String useDatabaseQuery = "USE " + DB_NAME;
             statement.executeUpdate(useDatabaseQuery);
+            
+            // User table
+            String createUsersTableQuery = "CREATE TABLE IF NOT EXISTS users ("
+                    + "id INT AUTO_INCREMENT PRIMARY KEY,"
+                    + "email VARCHAR(255) NOT NULL UNIQUE,"
+                    + "password VARCHAR(255) NOT NULL"
+                    + ")";
+            statement.executeUpdate(createUsersTableQuery);
 
-            // Create the table for short URLs if it doesn't exist
+            // Short urls table
             String createShortUrlsTableQuery = "CREATE TABLE IF NOT EXISTS short_urls ("
                     + "id INT AUTO_INCREMENT PRIMARY KEY,"
                     + "slug VARCHAR(255) NOT NULL,"
@@ -130,7 +183,7 @@ public class ApplicationDao {
                     + ")";
             statement.executeUpdate(createShortUrlsTableQuery);
 
-            // Create the table for click information if it doesn't exist
+            // Click info
             String createClicksTableQuery = "CREATE TABLE IF NOT EXISTS short_url_clicks ("
                     + "id INT AUTO_INCREMENT PRIMARY KEY,"
                     + "short_url_id INT,"
